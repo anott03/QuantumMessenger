d4ac75cd1cb5f19df558b37537832ca99d3e5859
anott03 <amitav_nott@ryecountryday.org>
2022-02-08 09:57:02 -0500

    feat(encode_qubits) - encode_qubits function - style: all function names are now snake case

--- api/BB84.py
+++ api/BB84.py
@@ -6,7 +6,7 @@ from qiskit import Aer, assemble, transpile
 sim = Aer.get_backend("aer_simulator")
 
 
-def getRandomNumbersQuantum(n: int) -> list:
+def get_random_numbers_quantum(n: int) -> list:
     qc = QuantumCircuit(1)
     qc.h(0)
     qc.measure_all()
@@ -18,7 +18,20 @@ def getRandomNumbersClassical(n: int) -> list:
     return [randint(0, 1) for _ in range(n)]
 
 
-def encodeQubit():
+def encode_qubits(bits: list, bases: list):
+    encodedBits = []
     # choose random basis (x or z)
+    # 0 --> X basis
+    # 1 --> Z bassi
+    if not bases:
+        bases = get_random_numbers_quantum(len(bits))
     # encode a qubit into superposition of chosen basis
-    pass
+    for bit, base in zip(bits, bases):
+        qc = QuantumCircuit(1, 1)
+        if bit == 1:
+            qc.x(0)
+        if base == 0:  # X basis
+            qc.h(0)
+        qc.barrier()  # not sure if we need this here...
+        encodedBits.append(qc)
+    return encodedBits
--- api/BB84_test.py
+++ api/BB84_test.py
@@ -1,8 +1,24 @@
-from BB84 import getRandomNumbersQuantum
+from BB84 import get_random_numbers_quantum, encode_qubits
 
-def test_getRandomNumbersQuantum():
+
+def test_result(name, status):
+    print(f"{status} ---------- {name}")
+
+
+def test_get_random_numbers_quantum():
     for i in range(1, 102, 10):
-        data = getRandomNumbersQuantum(i)
-        print(f"{len(data)}: {data}")
+        data = get_random_numbers_quantum(i)
+        assert len(data) == i
+    test_result("get_random_numbers_quantum", "PASS")
 
-test_getRandomNumbersQuantum()
+
+def test_encode_qubits():
+    bases = get_random_numbers_quantum(5)
+    bits = get_random_numbers_quantum(5)
+    encodedBits = encode_qubits(bits, bases)
+    print(encodedBits)
+    test_result("encode_qubits", "PASS")
+
+
+test_get_random_numbers_quantum()
+test_encode_qubits()
