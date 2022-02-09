90f6c6f7fa1b56bb8d3bd6d4e1c2d448a383f5f4
Rohan Malik <rohan_malik@ryecountryday.org>
2022-02-09 07:04:30 -0500

    tests for measure and prune

--- api/BB84_test.py
+++ api/BB84_test.py
@@ -1,4 +1,5 @@
-from BB84 import get_random_numbers_quantum, encode_qubits
+from BB84 import get_random_numbers_quantum, encode_qubits, measure_qubits, prune_invalid, sample_bits, bb84
+from random import sample
 
 
 def test_result(name, status):
@@ -20,5 +21,26 @@ def test_encode_qubits():
     test_result("encode_qubits", "PASS")
 
 
+def test_measure_qubits():
+    bases = get_random_numbers_quantum(5)
+    bits = get_random_numbers_quantum(5)
+    encodedBits = encode_qubits(bits, bases)
+    decodedBits = measure_qubits(encodedBits, bases)
+    print(decodedBits)
+    test_result("measure_qubits", "PASS")
+
+
+def test_prune_invalid():
+    bits = get_random_numbers_quantum(10)
+    bases1 = get_random_numbers_quantum(10)
+    bases2 = get_random_numbers_quantum(10)
+    pruned = prune_invalid(bases1, bases2, bits)
+    print(f"bases1: {bases1}\nbases2: {bases2}")
+    print(f"Original: {bits}")
+    print(f"New:      {pruned}")
+    test_result("prune_invalid", "PASS")
+
 test_get_random_numbers_quantum()
 test_encode_qubits()
+test_measure_qubits()
+test_prune_invalid()
