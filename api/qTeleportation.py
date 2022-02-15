11e86b65c82804c3e39f6879db325e12be831adb
Rohan Malik <rohan_malik@ryecountryday.org>
2022-02-15 06:55:02 -0500

    Fix bugs in quantum teleportation

--- api/qTeleportation.py
+++ api/qTeleportation.py
@@ -1,7 +1,7 @@
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 
 
-def quantumTeleport(nQubits: int, origin: int, destination: int, aux: int):
+def quantum_teleport(nQubits: int, origin: int, destination: int, aux: int):
     # returns a circuit that teleports one qubit state to another location using a helper qubit
     qr = QuantumRegister(nQubits)
     cl1 = ClassicalRegister(1)
@@ -11,8 +11,8 @@ def quantumTeleport(nQubits: int, origin: int, destination: int, aux: int):
     qc.cx(destination, aux)
     qc.cx(origin, aux)
     qc.h(origin)
-    qc.measure([0], [0])
-    qc.measure([1], [1])
-    qc.x(2).c_if(cl2, 1)
-    qc.z(2).c_if(cl1, 1)
+    qc.measure([origin], [0])
+    qc.measure([aux], [1])
+    qc.x(destination).c_if(cl2, 1)
+    qc.z(destination).c_if(cl1, 1)
     return qc
