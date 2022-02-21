e9c5916d9c740e30ec8e6f05e255e25dff6e49f4
anott03 <amitav_nott@ryecountryday.org>
2022-02-21 12:20:19 -0500

    finishing up initial tests

--- api/test/test_bb84.py
+++ api/test/test_bb84.py
@@ -1,6 +1,6 @@
 from BB84 import BB84, QuantumUtils
 from qiskit import Aer
-# from random import sample
+from random import sample
 
 
 class TestBB84:
@@ -26,22 +26,24 @@ class TestBB84:
         assert len(encoded_bits) == len(decoded_bits)
 
     # TODO: figure out how to do this test
-    #  def test_prune_invalid():
-        #  bits = get_random_numbers_quantum(10)
-        #  bases1 = get_random_numbers_quantum(10)
-        #  bases2 = get_random_numbers_quantum(10)
-        #  pruned = prune_invalid(bases1, bases2, bits)
-        #  print(f"bases1: {bases1}\nbases2: {bases2}")
-        #  print(f"Original: {bits}")
-        #  print(f"New:      {pruned}")
+    def test_prune_invalid(x):
+        sim = Aer.get_backend("aer_simulator")
+        bits = QuantumUtils.get_random_numbers(10, sim)
+        bases1 = QuantumUtils.get_random_numbers(10, sim)
+        bases2 = QuantumUtils.get_random_numbers(10, sim)
+        pruned = QuantumUtils.prune_invalid(bases1, bases2, bits)
+        print(f"bases1: {bases1}\nbases2: {bases2}")
+        print(f"Original: {bits}")
+        print(f"New:      {pruned}")
 
     # TODO: figure out how to do this test
-    #  def test_sample_bits():
-        #  bits = get_random_numbers_quantum(10)
-        #  sampleIndices = sample(range(len(bits)), 5)
-        #  sampled = sample_bits(bits, sampleIndices)
-        #  print(f"Sample: {sampled}")
-        #  print(f"Bits: {bits}")
+    def test_sample_bits(x):
+        bb84 = BB84()
+        bits = QuantumUtils.get_random_numbers(10, bb84.sim)
+        sampleIndices = sample(range(len(bits)), 5)
+        sampled = bb84.sample_bits(bits, sampleIndices)
+        print(f"Sample: {sampled}")
+        print(f"Bits: {bits}")
 
     def test_bb84(x):
         bb84 = BB84()
--- api/test/test_qTeleportation.py
+++ api/test/test_qTeleportation.py
@@ -1,16 +1,11 @@
-#  from qTeleportation import quantum_teleport
+from qTeleportation import quantum_teleport
 
 
-#  def test_result(name, status):
-    #  print(f"{status} ---------- {name}")
-
-
-#  def test_teleportation():
-    #  print("== Circuit 1: 3 qubits, teleport from 1 to 2 with 3 ==")
-    #  print(quantum_teleport(3, 0, 1, 2))
-    #  print("== Circuit 2: 8 qubits, teleport from 3 to 7 with 5 ==")
-    #  print(quantum_teleport(8, 2, 6, 4))
-    #  test_result("quantum_teleport", "PASS")
-
-
-#  test_teleportation()
+class TestQTeleportation:
+    # TODO: rethink this test?
+    # right now it passes simply by virtue of not throwing any errors
+    def test_teleportation(x):
+        print("== Circuit 1: 3 qubits, teleport from 1 to 2 with 3 ==")
+        print(quantum_teleport(3, 0, 1, 2))
+        print("== Circuit 2: 8 qubits, teleport from 3 to 7 with 5 ==")
+        print(quantum_teleport(8, 2, 6, 4))
