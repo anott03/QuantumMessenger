3869f6f5ada3ad672d44ecd5b8a617a113a753d8
Rohan Malik <rohan_malik@ryecountryday.org>
2022-03-22 17:41:39 -0400

    Make methods static + add new bb84 test

--- api/test/test_bb84.py
+++ api/test/test_bb84.py
@@ -1,23 +1,26 @@
-from BB84 import BB84, QuantumUtils
+from BB84 import BB84, ParallelBB84, QuantumUtils
 from qiskit import Aer
 from random import sample
 
 
 class TestBB84:
-    def test_get_random_numbers_quantum(x):
+    @staticmethod
+    def test_get_random_numbers_quantum():
         for i in range(1, 102, 10):
             sim = Aer.get_backend("aer_simulator")
             data = QuantumUtils.get_random_numbers(i, sim)
             assert len(data) == i
 
-    def test_encode_qubits(x):
+    @staticmethod
+    def test_encode_qubits():
         bb84 = BB84()
         bases = QuantumUtils.get_random_numbers(5, bb84.sim)
         bits = QuantumUtils.get_random_numbers(5, bb84.sim)
         encoded_bits = bb84.encode_qubits(bits, bases)
         assert len(encoded_bits) == len(bits)
 
-    def test_measure_qubits(x):
+    @staticmethod
+    def test_measure_qubits():
         bb84 = BB84()
         bits = QuantumUtils.get_random_numbers(10, bb84.sim)
         bases = QuantumUtils.get_random_numbers(10, bb84.sim)
@@ -26,7 +29,8 @@ class TestBB84:
         assert len(encoded_bits) == len(decoded_bits)
 
     # TODO: figure out how to do this test
-    def test_prune_invalid(x):
+    @staticmethod
+    def test_prune_invalid():
         sim = Aer.get_backend("aer_simulator")
         bits = QuantumUtils.get_random_numbers(10, sim)
         bases1 = QuantumUtils.get_random_numbers(10, sim)
@@ -37,7 +41,8 @@ class TestBB84:
         print(f"New:      {pruned}")
 
     # TODO: figure out how to do this test
-    def test_sample_bits(x):
+    @staticmethod
+    def test_sample_bits():
         bb84 = BB84()
         bits = QuantumUtils.get_random_numbers(10, bb84.sim)
         sampleIndices = sample(range(len(bits)), 5)
@@ -45,7 +50,16 @@ class TestBB84:
         print(f"Sample: {sampled}")
         print(f"Bits: {bits}")
 
-    def test_bb84(x):
+    @staticmethod
+    def test_bb84():
         bb84 = BB84()
         key = bb84.bb84(50)
         assert key is not None
+
+    @staticmethod
+    def test_parallel_bb84():
+        parallel_bb84 = ParallelBB84(5)
+        post_sender_state = parallel_bb84.sender_protocol()
+        results = parallel_bb84.receiver_protocol(post_sender_state)
+        print(f"Shared key: {results}")
+
