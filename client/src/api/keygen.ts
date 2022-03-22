a66b899d08b4a3b420a54c08f36d9d8ed408d75f
anott03 <amitav_nott@ryecountryday.org>
2022-03-23 00:37:14 +0100

    working on creating react hooks to make API calls

---
+++ client/src/api/keygen.ts
@@ -0,0 +1,9 @@
+function useQuantumKeyGen() {
+    const URI = "";
+    const opts = {};
+    fetch(URI, opts).then(res => res.json()).then(data => {
+        
+    })
+}
+
+export { useQuantumKeyGen }
