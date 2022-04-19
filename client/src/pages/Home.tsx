07ee51c135046867477a1a8108f90e3b34a9d544
anott03 <amitav_nott@ryecountryday.org>
2022-04-18 20:47:10 -0400

    completed mock home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -21,7 +21,29 @@ const Home = () => {
 
 
       <div className="home__body">
-        hello
+        <div className="sidebar">
+          <div className="contact selected">
+            <h3>New Message</h3>
+            <p>some text...</p>
+          </div>
+
+          <div className="contact">
+            <h3>Rohan Malik</h3>
+            <p>some text...</p>
+          </div>
+
+          <div className="contact">
+            <h3>Paco Martin</h3>
+            <p>some text...</p>
+          </div>
+        </div>
+
+        <div className="messages">
+          <form className="message-form">
+            <input type="text" placeholder="Enter Message Here"/>
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   );
