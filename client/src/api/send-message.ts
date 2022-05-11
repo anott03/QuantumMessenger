1911a2ba01c91c652b316bff3b403d619ce24f2c
Rohan Malik <rohan_malik@ryecountryday.org>
2022-05-10 20:17:54 -0400

    Small revisions for updated api structure

--- client/src/api/send-message.ts
+++ client/src/api/send-message.ts
@@ -12,11 +12,11 @@ function useSendMessage(): (messageId: String, message: String) => void {
       },
 
       body: JSON.stringify({
-        username: "test",
         user_id: "test",
         receiver_id: "test2",
         message_id: messageId,
-        message_content: message
+        message_content: message,
+        timestamp: Date.now()
       })
     }
 
